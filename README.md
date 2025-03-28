5b1571695164a3e09bdf7eb90829d5e1f0c09c97
Sridhar Ratnakumar <srid@srid.ca>
2025-03-28 13:35:26 -0400

    Adapt uv2nix's example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-nix-template
+
 WIP: A simple Nix project template for Python projects
+
+## Details
+
+Based on [uv2nix's hello-world example](https://github.com/pyproject-nix/uv2nix/tree/master/templates/hello-world). More to come!
---
+++ src/python_nix_template/__init__.py
@@ -0,0 +1,34 @@
+import os
+import socket
+import platform
+from colorama import init, Fore, Style
+
+def main():
+    # Initialize colorama for cross-platform compatibility
+    init()
+    print_colored_info()
+
+def get_system_info():
+    """
+    Retrieve system information including hostname, username, and OS name.
+    Returns a tuple containing these values.
+    """
+    hostname = socket.gethostname()
+    username = os.getlogin()
+    os_name = platform.system()
+    return hostname, username, os_name
+
+def print_colored_info():
+    """
+    Print system information in different colors:
+    - Hostname in red
+    - Username in green
+    - OS name in blue
+    """
+    # Get system information
+    hostname, username, os_name = get_system_info()
+    
+    # Print each piece of information in a distinct color
+    print(f"Hostname: {Fore.RED}{hostname}{Style.RESET_ALL}")
+    print(f"Username: {Fore.GREEN}{username}{Style.RESET_ALL}")
+    print(f"OS Name: {Fore.BLUE}{os_name}{Style.RESET_ALL}")
